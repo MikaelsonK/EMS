85db1f58c2da165f7de9b0fa468abfec3a3757bf
MikaelsonK <chloebelle.estilo20@gmail.com>
2025-02-25 15:01:55 +0800

    add testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import '@testing-library/jest-dom';
+
 
 function App() {
   const [count, setCount] = useState(0)
